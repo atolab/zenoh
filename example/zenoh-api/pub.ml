71f11853c8334d26d1fe25f425df8b180792bc37
Olivier Hecart <olivier.hecart@prismtech.com>
2018-08-02 17:41:43 +0200

    Improve api example

--- example/zenoh-api/pub.ml
+++ example/zenoh-api/pub.ml
@@ -5,10 +5,12 @@ open Apero
 
 let run peer = 
   let%lwt z = zopen "tcp/127.0.0.1:7447" in 
-  let%lwt pub = publish "/home1" z in 
-  let buf = Result.get (encode_string "MSG" (IOBuf.clear (IOBuf.create 1024))
-  >>> IOBuf.flip) in
-  let%lwt _ = stream buf pub in
+  let%lwt home1pub = publish "/home1" z in 
+  let%lwt res1pub = publish "/res1" z in 
+  let buf = Result.get (encode_string "HOME1_MSG1" (IOBuf.clear (IOBuf.create 1024)) >>> IOBuf.flip) in
+  let%lwt _ = stream buf home1pub in
+  let buf = Result.get (encode_string "RES1_MSG1" (IOBuf.clear (IOBuf.create 1024)) >>> IOBuf.flip) in
+  let%lwt _ = stream buf res1pub in
   let%lwt _ = Lwt_unix.sleep 1.0 in
   Lwt.return_unit
 
--- example/zenoh-api/sub.ml
+++ example/zenoh-api/sub.ml
@@ -5,12 +5,13 @@ open Apero
 
 let listener sub data src = 
   let (str, _) = Result.get @@ decode_string data in
-  Printf.printf "LISTENER [%s] RECIEVED FROM [%s] : %s\n%!" sub str src;
+  Printf.printf "LISTENER [%-8s] RECIEVED FROM [%-8s] : %s\n%!" sub src str;
   Lwt.return_unit
   
 
 let run peer = 
   let%lwt z = zopen "tcp/127.0.0.1:7447" in 
+  let%lwt _ = subscribe "/*" (listener "/*") z in 
   let%lwt _ = subscribe "/home*" (listener "/home*") z in 
   let%lwt _ = Lwt_unix.sleep 3000.0 in 
   Lwt.return_unit
