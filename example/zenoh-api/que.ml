01c472030e119896d13650553a9b06c47b85efc1
Olivier Hécart <olivier.hecart@adlinktech.com>
2018-11-21 18:51:42 +0100

    Add storage and query examples

---
+++ example/zenoh-api/que.ml
@@ -0,0 +1,16 @@
+open Zenoh
+open Apero
+
+let listener sub data src = 
+  let (str, _) = Result.get @@ decode_string data in
+  Printf.printf "LISTENER [%-8s] RECIEVED FROM [%-8s] : %s\n%!" sub src str;
+  Lwt.return_unit
+
+let run = 
+  let%lwt z = zopen "tcp/127.0.0.1:7447" in 
+  let%lwt _ = query "/home1**" "" (listener "/home1**") z in 
+  let%lwt _ = Lwt_unix.sleep 3000.0 in 
+  Lwt.return_unit
+
+let () = 
+  Lwt_main.run @@ run
