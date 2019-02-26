26a6ee3db373384c012d6180d11acc10391e6e6d
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-02-26 14:44:46 +0100

    Improve query example

--- example/zenoh-api/query.ml
+++ example/zenoh-api/query.ml
@@ -9,19 +9,26 @@ let peer = match Array.length Sys.argv with
 let handler = function
   | StorageData rep ->
     let str = decode_string rep.data in
-    Printf.printf "QUERY HANDLER RECIEVED FROM STORAGE [%-16s:%02i] RESOURCE [%-20s] : %s\n%!" (Abuf.hexdump rep.stoid) rep.rsn rep.resname str;
+    Printf.printf "  QUERY HANDLER RECIEVED FROM STORAGE [%-16s:%02i] RESOURCE [%-20s] : %s\n%!" (Abuf.hexdump rep.stoid) rep.rsn rep.resname str;
     Lwt.return_unit
   | StorageFinal rep -> 
-    Printf.printf "QUERY HANDLER RECIEVED FROM STORAGE [%-16s:%02i] FINAL\n%!" (Abuf.hexdump rep.stoid) rep.rsn;
+    Printf.printf "  QUERY HANDLER RECIEVED FROM STORAGE [%-16s:%02i] FINAL\n%!" (Abuf.hexdump rep.stoid) rep.rsn;
     Lwt.return_unit
   | ReplyFinal -> 
-    Printf.printf "QUERY HANDLER RECIEVED GLOBAL FINAL\n%!";
+    Printf.printf "  QUERY HANDLER RECIEVED GLOBAL FINAL\n%!";
     Lwt.return_unit
 
 let run = 
   let%lwt z = zopen peer in    
+  Printf.printf "QUERY /home1/** :\n%!";
   let%lwt _ = query "/home1/**" "" handler z in 
-  let%lwt _ = lquery "/home1/**" "" z >|= List.iter (fun (k,_) -> Printf.printf "%s\n" k) in
+
+  let%lwt _ = Lwt_unix.sleep 0.2 in
+
+  Printf.printf "\n%!";
+  Printf.printf "LQUERY /home1/** :\n%!";
+  let%lwt _ = lquery "/home1/**" "" z >|= List.iter (fun (k,v) -> Printf.printf "  RECEIVED RESOURCE [%-20s] : %s\n%!" k (decode_string v)) in
+
   Lwt_unix.sleep 1.0
 
 
