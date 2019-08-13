042e8d30c3240869949477bc80c253b86075b46b
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-08-13 17:30:13 +0200

    Add router-lib example

---
+++ example/router-lib/router_pub.ml
@@ -0,0 +1,28 @@
+open Zenoh
+open Apero
+
+let run tcpport peers strength usersfile bufn timestamp = 
+  Lwt_main.run (
+    let _ = Zrouter.run tcpport peers strength usersfile bufn timestamp in
+    let%lwt z = zopen "" in 
+    let%lwt pub = publish z "/home2" in 
+    let rec publish i = 
+      let buf = Abuf.create 1024 in
+      encode_string ("HOME2_MSG" ^ Pervasives.string_of_int i) buf;
+      let%lwt _ = stream pub buf in
+      let%lwt _ = Lwt_unix.sleep 1.0 in
+      publish (i + 1)
+    in
+    publish 0
+  )
+
+let () = 
+  let open Cmdliner in
+  let _ = Term.(eval (const run $ 
+                      Zrouter.tcpport $ 
+                      Zrouter.peers $ 
+                      Zrouter.strength $ 
+                      Zrouter.users $ 
+                      Zrouter.bufn $
+                      Zrouter.timestamp, Term.info "router_pub")) 
+  in ()
