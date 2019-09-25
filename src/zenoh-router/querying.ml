8d443b1aefec388143e2442c5c550f74f2276c39
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-09-25 12:54:56 +0200

    zenohd forwards admin queries to plugins

--- src/zenoh-router/querying.ml
+++ src/zenoh-router/querying.ml
@@ -40,7 +40,7 @@ let forward_query_to pe q = List.map (fun s -> forward_query_to_session pe q s)
 let forward_amdin_query pe sid q = 
   let open Lwt.Infix in 
   let faces = SIDMap.bindings pe.smap 
-    |> List.filter (fun (id, s) -> id <> sid && Session.is_broker s) 
+    |> List.filter (fun (id, s) -> id <> sid && (id < Id.zero || Session.is_broker s))
     |> List.split |> fst
   in 
   Lwt.join @@ forward_query_to pe q faces >>= fun _ -> Lwt.return faces
