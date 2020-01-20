9063782b4539f08fce3ead7ca77f3b3d983408ff
Ivan Paez <ivan.paez@adlinktech.com>
2020-01-20 15:50:54 +0100

    Added EPL-2.0, Apache-2.0 LICENSE header to source files

--- src/zenoh-net-ocaml/zenoh_local_router.ml
+++ src/zenoh-net-ocaml/zenoh_local_router.ml
@@ -1,3 +1,21 @@
+(*
+ * Copyright (c) 2014, 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *
+ * Contributors:
+ * Angelo Corsaro, <angelo.corsaro@adlinktech.com>
+ * Olivier Hecart, <olivier.hecart@adlinktech.com>
+ * Julien Enoch, <julien.enoch@adlinktech.com>.
+ *)
 type local_sex = (Frame.Frame.t Lwt_stream.t * Frame.Frame.t Lwt_stream.bounded_push)
 type local_sex_listener = local_sex -> unit Lwt.t
 
--- src/zenoh-storages/zenoh-storages-be/zenoh-storages-be-influxdb/zenoh_storages_be_influxdb.ml
+++ src/zenoh-storages/zenoh-storages-be/zenoh-storages-be-influxdb/zenoh_storages_be_influxdb.ml
@@ -1,3 +1,21 @@
+(*
+ * Copyright (c) 2014, 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *
+ * Contributors:
+ * Angelo Corsaro, <angelo.corsaro@adlinktech.com>
+ * Olivier Hecart, <olivier.hecart@adlinktech.com>
+ * Julien Enoch, <julien.enoch@adlinktech.com>.
+ *)
 open Apero
 open Lwt.Infix
 open Zenoh_types
