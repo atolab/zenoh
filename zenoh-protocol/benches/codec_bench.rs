19f911e7d53b82731490a4c3c61b6e1aa4cd6949
Angelo Corsaro <angelo@icorsaro.net>
2020-01-22 14:56:18 -0800

    removed c3 codec along with read/write for u16/u32/u64

--- zenoh-protocol/benches/codec_bench.rs
+++ zenoh-protocol/benches/codec_bench.rs
@@ -8,13 +8,6 @@ use zenoh_protocol::io::rwbuf::{RWBuf,OutOfBounds};
 use rand::distributions::{Distribution, Standard};
 
 
-fn unsafe_write_u32((v, buf): (u32, &mut RWBuf)) -> Result<(), OutOfBounds> {  
-  
-  let bs = unsafe {std::mem::transmute::<&u32, &[u8; 4]>(&v)};  
-  buf.write_bytes(bs).map(|_| ())
-}
-
-
 fn bench_zint_write((v, buf): (u64, &mut RWBuf)) -> Result<(), OutOfBounds> {  
   buf.write_zint(v).map(|_| ())
 }
@@ -28,18 +21,6 @@ fn bench_zint_write_three((v, buf): (&[u64; 3], &mut RWBuf)) -> Result<(), OutOf
   buf.write_zint(v[0])?;
   buf.write_zint(v[1])?;
   buf.write_zint(v[2])
-}
-
-fn bench_write_one_c3((n, buf): (u64, &mut RWBuf)) -> Result<(), OutOfBounds> {
-  buf.write_one_c3(n)
-}
-
-fn bench_write_two_c3((xs, buf): (&[u64;2], &mut RWBuf)) -> Result<(), OutOfBounds> {
-  buf.write_two_c3(xs)
-}
-
-fn bench_write_three_c3((xs, buf): (&[u64;3], &mut RWBuf)) -> Result<(), OutOfBounds> {
-  buf.write_three_c3(xs)
 }
 
 fn bench_one_zint_codec((v, buf): (u64, &mut RWBuf)) -> Result<(), OutOfBounds> {  
@@ -63,20 +44,6 @@ fn bench_three_zint_codec((v, buf): (&[u64;3], &mut RWBuf)) -> Result<(), OutOfB
   buf.read_zint().map(|_| ())  
 }
 
-fn bench_one_c3_codec((v, ns, buf): (u64, &mut [u64;4], &mut RWBuf)) -> Result<(), OutOfBounds> {  
-  buf.write_one_c3(v)?;  
-  buf.read_c3(ns).map(|_| ())  
-}
-
-fn bench_two_c3_codec((vs, ns, buf): (&[u64;2], &mut[u64;4], &mut RWBuf)) -> Result<(), OutOfBounds> {  
-  buf.write_two_c3(vs)?;
-  buf.read_c3(ns).map(|_| ())  
-}
-
-fn bench_three_c3_codec((vs, ns, buf): (&[u64;3], &mut[u64;4], &mut RWBuf)) -> Result<(), OutOfBounds> {  
-  buf.write_three_c3(vs)?;
-  buf.read_c3(ns).map(|_| ())  
-}
 
 fn bench_write_10bytes1((v, buf): (u8, &mut RWBuf)) -> Result<(), OutOfBounds> {
   buf.write(v)?;
@@ -104,309 +71,15 @@ fn criterion_benchmark(c: &mut Criterion) {
       buf.clear();
       }));
 
-    c.bench_function(&format!("bench_one_c3_codec {}", len), |b| b.iter(|| {
-      let _ = bench_one_c3_codec(black_box((rs3[0], &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-    
     c.bench_function(&format!("bench_two_zint_codec {}", len), |b| b.iter(|| {
       let _ = bench_two_zint_codec(black_box((&rs2, &mut buf)));
       buf.clear();
       }));
 
-    c.bench_function("bench_two_c3_codec u8", |b| b.iter(|| {
-      let _ = bench_two_c3_codec(black_box((&rs2, &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-
     c.bench_function("bench_three_zint_codec u8", |b| b.iter(|| {
       let _ = bench_three_zint_codec(black_box((&rs3, &mut buf)));
       buf.clear();
       }));
-
-    c.bench_function("bench_three_c3_codec u8", |b| b.iter(|| {
-      let _ = bench_three_c3_codec(black_box((&rs3, &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-    // c.bench_function("bench_zint_write u8", |b| b.iter(|| {
-    //   let _ = bench_zint_write(black_box((rs3[0], &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_one_c3 u8", |b| b.iter(|| {
-    //   let _ = bench_write_one_c3(black_box((rs3[0], &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // c.bench_function("bench_zint_write_two u8", |b| b.iter(|| {
-    //   let _ = bench_zint_write_two(black_box((&rs2, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_two_c3 u8", |b| b.iter(|| {
-    //   let _ = bench_write_two_c3(black_box((&rs2, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // c.bench_function("bench_zint_write_three u8", |b| b.iter(|| {
-    //   let _ = bench_zint_write_three(black_box((&rs3, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_three_c3 u8", |b| b.iter(|| {
-    //   let _ = bench_write_three_c3(black_box((&rs3, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-      ///////////////// u16 ////////////////
-    rs3 = [u64::from(rand::random::<u16>()), u64::from(rand::random::<u16>()), u64::from(rand::random::<u16>())];     
-    rs2 = [u64::from(rand::random::<u16>()), u64::from(rand::random::<u16>())];
-
-    c.bench_function("bench_one_zint_codec u16", |b| b.iter(|| {
-      let _ = bench_one_zint_codec(black_box((rs3[0], &mut buf)));
-      buf.clear();
-      }));
-
-    c.bench_function("bench_one_c3_codec u16", |b| b.iter(|| {
-      let _ = bench_one_c3_codec(black_box((rs3[0], &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-    
-    c.bench_function("bench_two_zint_codec u16", |b| b.iter(|| {
-      let _ = bench_two_zint_codec(black_box((&rs2, &mut buf)));
-      buf.clear();
-      }));
-
-    c.bench_function("bench_two_c3_codec u16", |b| b.iter(|| {
-      let _ = bench_two_c3_codec(black_box((&rs2, &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-
-    c.bench_function("bench_three_zint_codec u16", |b| b.iter(|| {
-      let _ = bench_three_zint_codec(black_box((&rs3, &mut buf)));
-      buf.clear();
-      }));
-
-    c.bench_function("bench_three_c3_codec u16", |b| b.iter(|| {
-      let _ = bench_three_c3_codec(black_box((&rs3, &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-      
-    //   c.bench_function("bench_zint_write u16", |b| b.iter(|| {
-    //   let _ = bench_zint_write(black_box((rs3[0], &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_one_c3 u16", |b| b.iter(|| {
-    //   let _ = bench_write_one_c3(black_box((rs3[0], &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // c.bench_function("bench_zint_write_two u16", |b| b.iter(|| {
-    //   let _ = bench_zint_write_two(black_box((&rs2, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_two_c3 u16", |b| b.iter(|| {
-    //   let _ = bench_write_two_c3(black_box((&rs2, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // c.bench_function("bench_zint_write_three u16", |b| b.iter(|| {
-    //   let _ = bench_zint_write_three(black_box((&rs3, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_three_c3 u16", |b| b.iter(|| {
-    //   let _ = bench_write_three_c3(black_box((&rs3, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-
-    ///////////////// u32 ////////////////
-    rs3 = [u64::from(rand::random::<u32>()), u64::from(rand::random::<u32>()), u64::from(rand::random::<u32>())];     
-    rs2 = [u64::from(rand::random::<u32>()), u64::from(rand::random::<u32>())];
-
-
-    c.bench_function("bench_one_zint_codec u32", |b| b.iter(|| {
-      let _ = bench_one_zint_codec(black_box((rs3[0], &mut buf)));
-      buf.clear();
-      }));
-
-    c.bench_function("bench_one_c3_codec u32", |b| b.iter(|| {
-      let _ = bench_one_c3_codec(black_box((rs3[0], &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-    
-    c.bench_function("bench_two_zint_codec u32", |b| b.iter(|| {
-      let _ = bench_two_zint_codec(black_box((&rs2, &mut buf)));
-      buf.clear();
-      }));
-
-    c.bench_function("bench_two_c3_codec u32", |b| b.iter(|| {
-      let _ = bench_two_c3_codec(black_box((&rs2, &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-
-    c.bench_function("bench_three_zint_codec u32", |b| b.iter(|| {
-      let _ = bench_three_zint_codec(black_box((&rs3, &mut buf)));
-      buf.clear();
-      }));
-
-    c.bench_function("bench_three_c3_codec u32", |b| b.iter(|| {
-      let _ = bench_three_c3_codec(black_box((&rs3, &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-
-    //   c.bench_function("bench_zint_write u32", |b| b.iter(|| {
-    //   let _ = bench_zint_write(black_box((rs3[0], &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_one_c3 u32", |b| b.iter(|| {
-    //   let _ = bench_write_one_c3(black_box((rs3[0], &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // c.bench_function("bench_zint_write_two u32", |b| b.iter(|| {
-    //   let _ = bench_zint_write_two(black_box((&rs2, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_two_c3 u32", |b| b.iter(|| {
-    //   let _ = bench_write_two_c3(black_box((&rs2, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // c.bench_function("bench_zint_write_three u32", |b| b.iter(|| {
-    //   let _ = bench_zint_write_three(black_box((&rs3, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_three_c3 u32", |b| b.iter(|| {
-    //   let _ = bench_write_three_c3(black_box((&rs3, &mut buf)));
-    //   buf.clear();
-    //   }));      
-
-    ///////////////// u64 ////////////////
-      rs3 = [u64::from(rand::random::<u64>()), u64::from(rand::random::<u64>()), u64::from(rand::random::<u64>())];     
-      rs2 = [u64::from(rand::random::<u64>()), u64::from(rand::random::<u64>())];
-    
-    c.bench_function("bench_one_zint_codec u64", |b| b.iter(|| {
-      let _ = bench_one_zint_codec(black_box((rs3[0], &mut buf)));
-      buf.clear();
-      }));
-
-    c.bench_function("bench_one_c3_codec u64", |b| b.iter(|| {
-      let _ = bench_one_c3_codec(black_box((rs3[0], &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-    
-    c.bench_function("bench_two_zint_codec u64", |b| b.iter(|| {
-      let _ = bench_two_zint_codec(black_box((&rs2, &mut buf)));
-      buf.clear();
-      }));
-
-    c.bench_function("bench_two_c3_codec u64", |b| b.iter(|| {
-      let _ = bench_two_c3_codec(black_box((&rs2, &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-
-    c.bench_function("bench_three_zint_codec u64", |b| b.iter(|| {
-      let _ = bench_three_zint_codec(black_box((&rs3, &mut buf)));
-      buf.clear();
-      }));
-
-    c.bench_function("bench_three_c3_codec u64", |b| b.iter(|| {
-      let _ = bench_three_c3_codec(black_box((&rs3, &mut ns, &mut buf)));
-      buf.clear();
-      }));  
-    //   c.bench_function("bench_zint_write u64", |b| b.iter(|| {
-    //   let _ = bench_zint_write(black_box((rs3[0], &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_one_c3 u64", |b| b.iter(|| {
-    //   let _ = bench_write_one_c3(black_box((rs3[0], &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // c.bench_function("bench_zint_write_two u64", |b| b.iter(|| {
-    //   let _ = bench_zint_write_two(black_box((&rs2, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_two_c3 u64", |b| b.iter(|| {
-    //   let _ = bench_write_two_c3(black_box((&rs2, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // c.bench_function("bench_zint_write_three u64", |b| b.iter(|| {
-    //   let _ = bench_zint_write_three(black_box((&rs3, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_write_three_c3 u64", |b| b.iter(|| {
-    //   let _ = bench_write_three_c3(black_box((&rs3, &mut buf)));
-    //   buf.clear();
-    //   }));      
-    // rs = [u64::from(rand::random::<u16>(), u64::from(rand::random::<u16>(), u64::from(rand::random::<u16>())];     
-    // c.bench_function("bench_zint_write2 u8", |b| b.iter(|| {
-    //   let _ = bench_zint_write(black_box((0x3f, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_zint_read u8", |b| b.iter(|| {
-    //   let _ = bench_zint_read(black_box(&mut buf));
-    //   buf.clear();
-    //   }));      
-    // c.bench_function("zint_codec u8", |b| b.iter(|| {
-    //   let _ = bench_zint_codec(black_box((a, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // // u16 
-    // a = u64::from(rand::random::<u16>());     
-    // c.bench_function("bench_zint_write u16", |b| b.iter(|| {
-    //   let _ = bench_zint_write(black_box((a, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_zint_write2 u16", |b| b.iter(|| {
-    //   let _ = bench_zint_write(black_box((0x1fff, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_zint_read u16 ", |b| b.iter(|| {
-    //   let _ = bench_zint_read(black_box(&mut buf));
-    //   buf.clear();
-    //   }));      
-    // c.bench_function("zint_codec u16", |b| b.iter(|| {
-    //   let _ = bench_zint_codec(black_box((a, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // // u32
-    // a = u64::from(rand::random::<u32>());
-    //   c.bench_function("bench_zint_write u32", |b| b.iter(|| {
-    //   let _ = bench_zint_write(black_box((a, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // // unsafe_write_u32 
-    // let x = u32::from(rand::random::<u32>());
-    // c.bench_function("unsafe_write_u32 u32", |b| b.iter(|| {
-    //   let _ = unsafe_write_u32(black_box((x, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_zint_read u32", |b| b.iter(|| {
-    //   let _ = bench_zint_read(black_box(&mut buf));
-    //   buf.clear();
-    //   }));      
-    // c.bench_function("zint_codec u32 ", |b| b.iter(|| {
-    //   let _ = bench_zint_codec(black_box((a, &mut buf)));
-    //   buf.clear();
-    //   }));
-
-    // // u64
-    //   a = u64::from(rand::random::<u64>());
-    //   c.bench_function("bench_zint_write u64", |b| b.iter(|| {
-    //   let _ = bench_zint_write(black_box((a, &mut buf)));
-    //   buf.clear();
-    //   }));
-    // c.bench_function("bench_zint_read u64", |b| b.iter(|| {
-    //   let _ = bench_zint_read(black_box(&mut buf));
-    //   buf.clear();
-    //   }));      
-    // c.bench_function("zint_codec u64", |b| b.iter(|| {
-    //   let _ = bench_zint_codec(black_box((a, &mut buf)));
-    //   buf.clear();
-    //   }));
 
     let r4 = rand::random::<u8>();
     c.bench_function("bench_write_10bytes1", |b| b.iter(|| {
