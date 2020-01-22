bad6e95a8d9c282b3c39914636f737cb2d94efd7
Angelo Corsaro <angelo@icorsaro.net>
2020-01-22 14:45:40 -0800

    added delcarations

--- zenoh-protocol/src/proto/decl.rs
+++ zenoh-protocol/src/proto/decl.rs
@@ -1,5 +1,5 @@
 
-use crate::core::{ZInt, Property};
+use crate::core::{ZInt, ResKey };
 
 pub mod id {
   pub const RESOURCE            :  u8 =  0x01;
@@ -15,40 +15,125 @@ pub mod id {
   pub const FORGET_EVAL         :  u8 =  0x15;      
 }
 
+#[derive(Debug, Clone)]
+pub enum SubMode {
+    Push,
+    Pull,
+    PeriodicPush { origin: ZInt, period: ZInt, duration: ZInt },
+    PeriodicPull { origin: ZInt, period: ZInt, duration: ZInt }
+}
 
 #[derive(Debug, Clone)]
 pub enum Declaration {
     ///  7 6 5 4 3 2 1 0
     /// +-+-+-+-+-+-+-+-+
-    /// |C|P|X| RESOURCE|
+    /// |X|X|X| RESOURCE|
     /// +---------------+
     /// ~      RID      ~
     /// +---------------+
-    /// ~    Resource   ~
+    /// ~ Resource Key ~
     /// +---------------+
-    /// ~  [Property]   ~
-    /// +---------------+
-    ///
-    /// If C=1 then the textual representation of the resource can be removed
-    /// RID represents the compact ID associated with the resource and is always
-    /// included
+    ///    
+    /// @Olivier, the idea would be to be able to declare a 
+    /// resource using an ID to avoid sending the prefix. 
+    /// If we do this however, we open the door to receiving declaration
+    /// that may try to redefine an Id... Which BTW may not be so bad, as 
+    /// we could use this instead as the rebind. Thoughts?
     Resource {
         rid: ZInt,
-        resource: Option<String>,
-        properties: Vec<Property>
+        key: ResKey,        
     },
 
     ///  7 6 5 4 3 2 1 0
     /// +-+-+-+-+-+-+-+-+
-    /// |C|S|P|  PUBSUB |
+    /// |X|X|X|  F_RES  |
     /// +---------------+
-    /// ~      RID      |
+    /// ~      RID      ~
     /// +---------------+
-    /// ~    Resource   ~
+    ForgetResource {
+        rid: ZInt
+    },
+
+    ///  7 6 5 4 3 2 1 0
+    /// +-+-+-+-+-+-+-+-+
+    /// |X|X|X|   PUB   |
     /// +---------------+
-    PubSub {
-        ps_mask : u8,
-        rid: ZInt,
-        resource: Option<String>
+    /// ~      ResKey   ~
+    /// +---------------+
+    Publisher {
+        key: ResKey
+    },
+
+    ///  7 6 5 4 3 2 1 0
+    /// +-+-+-+-+-+-+-+-+
+    /// |X|X|X|   F_PUB   |
+    /// +---------------+
+    /// ~      ResKey   ~
+    /// +---------------+
+    ForgetPublisher {
+        key: ResKey
+    },
+
+    ///  7 6 5 4 3 2 1 0
+    /// +-+-+-+-+-+-+-+-+
+    /// |X|X|M|   SUB   |
+    /// +---------------+
+    /// ~      ResKey   ~
+    /// +---------------+
+    /// ~    SubMode    ~ if M==1
+    /// +---------------+
+    Subscriber {
+        key: ResKey,
+        mode: SubMode
+    },
+
+    ///  7 6 5 4 3 2 1 0
+    /// +-+-+-+-+-+-+-+-+
+    /// |X|X|X|   F_SUB   |
+    /// +---------------+
+    /// ~      ResKey   ~
+    /// +---------------+
+    ForgetSubscriber {
+        key: ResKey
+    },
+
+    ///  7 6 5 4 3 2 1 0
+    /// +-+-+-+-+-+-+-+-+
+    /// |X|X|X|  STORE  |
+    /// +---------------+
+    /// ~     ResKey    ~
+    /// +---------------+
+    Storage {
+        key: ResKey
+    },
+
+    ///  7 6 5 4 3 2 1 0
+    /// +-+-+-+-+-+-+-+-+
+    /// |X|X|X| F_STORE |
+    /// +---------------+
+    /// ~      ResKey   ~
+    /// +---------------+
+    ForgetStorage {
+        key: ResKey
+    },
+
+    ///  7 6 5 4 3 2 1 0
+    /// +-+-+-+-+-+-+-+-+
+    /// |X|X|X|  EVAL   |
+    /// +---------------+
+    /// ~      ResKey   ~
+    /// +---------------+
+    Eval {
+        key: ResKey
+    },
+
+    ///  7 6 5 4 3 2 1 0
+    /// +-+-+-+-+-+-+-+-+
+    /// |X|X|X| F_EVAL  |
+    /// +---------------+
+    /// ~      ResKey   ~
+    /// +---------------+
+    ForgetEval {
+        key: ResKey
     }
 }
