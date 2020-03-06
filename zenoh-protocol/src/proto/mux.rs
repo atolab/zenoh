d0e84fa08d1e8ecc8a3c6b0be6276002b14a0e73
Olivier Hécart <olivier.hecart@adlinktech.com>
2020-03-06 11:44:29 +0100

    Mux takes a generic MsgHandler instead of an explicit Session

--- zenoh-protocol/src/proto/mux.rs
+++ zenoh-protocol/src/proto/mux.rs
@@ -6,107 +6,108 @@ use crate::proto::{
     Message, SubMode, Declaration, 
     Primitives, MessageKind, QueryTarget, 
     QueryConsolidation, ReplyContext, ReplySource};
-use crate::session::Session;
+use crate::session::MsgHandler;
 
-pub struct Mux {
-    session: Session,
+pub struct Mux<T: MsgHandler + Send + Sync + ?Sized> {
+    handler: Arc<T>,
 }
 
-impl Mux {
-    pub fn new(session: Session) -> Mux {
-        Mux {session: session,}
+impl<T: MsgHandler + Send + Sync + ?Sized> Mux<T> {
+    pub fn new(handler: Arc<T>) -> Mux<T> {
+        Mux {handler}
     }
 }
 
+#[allow(unused_must_use)] // TODO
 #[async_trait]
-impl Primitives for Mux {
+impl<T: MsgHandler + Send + Sync + ?Sized> Primitives for Mux<T> {
     async fn resource(&self, rid: &u64, reskey: &ResKey) {
         let mut decls = Vec::new();
         decls.push(Declaration::Resource{rid: *rid, key: reskey.clone()});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
 
     async fn forget_resource(&self, rid: &u64) {
         let mut decls = Vec::new();
         decls.push(Declaration::ForgetResource{rid: *rid});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
     
     async fn subscriber(&self, reskey: &ResKey, mode: &SubMode) {
         let mut decls = Vec::new();
         decls.push(Declaration::Subscriber{key: reskey.clone(), mode: mode.clone()});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
 
     async fn forget_subscriber(&self, reskey: &ResKey) {
         let mut decls = Vec::new();
         decls.push(Declaration::ForgetSubscriber{key: reskey.clone()});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
     
     async fn publisher(&self, reskey: &ResKey) {
         let mut decls = Vec::new();
         decls.push(Declaration::Publisher{key: reskey.clone()});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
 
     async fn forget_publisher(&self, reskey: &ResKey) {
         let mut decls = Vec::new();
         decls.push(Declaration::ForgetPublisher{key: reskey.clone()});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
     
     async fn storage(&self, reskey: &ResKey) {
         let mut decls = Vec::new();
         decls.push(Declaration::Storage{key: reskey.clone()});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
 
     async fn forget_storage(&self, reskey: &ResKey) {
         let mut decls = Vec::new();
         decls.push(Declaration::ForgetStorage{key: reskey.clone()});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
     
     async fn eval(&self, reskey: &ResKey) {
         let mut decls = Vec::new();
         decls.push(Declaration::Eval{key: reskey.clone()});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
 
     async fn forget_eval(&self, reskey: &ResKey) {
         let mut decls = Vec::new();
         decls.push(Declaration::ForgetEval{key: reskey.clone()});
-        self.session.send(Arc::new(Message::make_declare(
-            0, decls, None, None))).await;
+        self.handler.handle_message(Message::make_declare(
+            0, decls, None, None)).await;
     }
 
     async fn data(&self, reskey: &ResKey, info: &Option<ArcSlice>, payload: &ArcSlice) {
-        self.session.send(Arc::new(Message::make_data(
-            MessageKind::FullMessage, true, 0, reskey.clone(), info.clone(), payload.clone(), None, None, None))).await;
+        self.handler.handle_message(Message::make_data(
+            MessageKind::FullMessage, true, 0, reskey.clone(), info.clone(), payload.clone(), None, None, None)).await;
     }
 
     async fn query(&self, reskey: &ResKey, predicate: &String, qid: &ZInt, target: &Option<QueryTarget>, consolidation: &QueryConsolidation) {
-        self.session.send(Arc::new(Message::make_query(
-            0, reskey.clone(), predicate.clone(), *qid, target.clone(), consolidation.clone(), None, None))).await;
+        self.handler.handle_message(Message::make_query(
+            0, reskey.clone(), predicate.clone(), *qid, target.clone(), consolidation.clone(), None, None)).await;
     }
 
     async fn reply(&self, qid: &ZInt, source: &ReplySource, replierid: &Option<PeerId>, reskey: &ResKey, info: &Option<ArcSlice>, payload: &ArcSlice) {
-        self.session.send(Arc::new(Message::make_data(
+        self.handler.handle_message(Message::make_data(
             MessageKind::FullMessage, true, 0, reskey.clone(), info.clone(), payload.clone(), 
-            Some(ReplyContext::make(*qid, source.clone(), replierid.clone())), None, None))).await;
+            Some(ReplyContext::make(*qid, source.clone(), replierid.clone())), None, None)).await;
     }
 
     async fn pull(&self, is_final: bool, reskey: &ResKey, pull_id: &ZInt, max_samples: &Option<ZInt>) {
-        self.session.send(Arc::new(Message::make_pull(is_final, 0, reskey.clone(), *pull_id, max_samples.clone(), None, None))).await;
+        self.handler.handle_message(Message::make_pull(is_final, 0, reskey.clone(), *pull_id, max_samples.clone(), None, None)).await;
     }
 }
