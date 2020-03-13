defd2d91049965e6605849c9902794e116b4a5f1
Olivier Hécart <olivier.hecart@adlinktech.com>
2020-03-13 13:15:37 +0100

    Subscriptions propagation

--- zenoh-router/benches/tables_bench.rs
+++ zenoh-router/benches/tables_bench.rs
@@ -4,7 +4,7 @@ extern crate zenoh_router;
 use async_std::task;
 use async_std::sync::Arc;
 use criterion::{Criterion, BenchmarkId};
-use zenoh_protocol::proto::Mux;
+use zenoh_protocol::proto::{Mux, SubMode, WhatAmI};
 use zenoh_protocol::session::DummyHandler;
 use zenoh_router::routing::tables::Tables;
 
@@ -13,18 +13,18 @@ fn tables_bench(c: &mut Criterion) {
     let tables = Tables::new();
     let primitives = Arc::new(Mux::new(Arc::new(DummyHandler::new())));
 
-    let sex0 = Tables::declare_session(&tables, primitives.clone()).await;
+    let sex0 = Tables::declare_session(&tables, WhatAmI::Client, primitives.clone()).await;
     Tables::declare_resource(&tables, &sex0, 1, 0, "/bench/tables").await;
     Tables::declare_resource(&tables, &sex0, 2, 0, "/bench/tables/*").await;
 
-    let sex1 = Tables::declare_session(&tables, primitives.clone()).await;
+    let sex1 = Tables::declare_session(&tables, WhatAmI::Client, primitives.clone()).await;
 
     let mut tables_bench = c.benchmark_group("tables_bench");
 
     for p in [8, 32, 256, 1024, 8192].iter() {
       for i in 1..(*p) {
         Tables::declare_resource(&tables, &sex1, i, 0, &["/bench/tables/AA", &i.to_string()].concat()).await;
-        Tables::declare_subscription(&tables, &sex1, i, "").await;
+        Tables::declare_subscription(&tables, &sex1, i, "", &SubMode::Push).await;
       }
 
       tables_bench.bench_function(BenchmarkId::new("direct_route", p), |b| b.iter(|| {
