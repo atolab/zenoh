1c33fda5608bcfbb5c759e7897d167fda90d19e1
Olivier Hécart <olivier.hecart@adlinktech.com>
2020-03-17 11:21:42 +0100

    Add primitives based executables

---
+++ zenoh-router/src/bin/peer.rs
@@ -0,0 +1,109 @@
+use async_std::task;
+use async_std::sync::Arc;
+use async_trait::async_trait;
+use rand::RngCore;
+use zenoh_protocol::core::{PeerId, ResKey, ZInt};
+use zenoh_protocol::io::ArcSlice;
+use zenoh_protocol::proto::WhatAmI;
+use zenoh_protocol::proto::{Primitives, SubMode, QueryConsolidation, QueryTarget, ReplySource};
+use zenoh_protocol::session::SessionManager;
+use zenoh_router::routing::tables::TablesHdl;
+
+pub struct PrintPrimitives {
+}
+
+#[async_trait]
+impl Primitives for PrintPrimitives {
+
+    async fn resource(&self, rid: &ZInt, reskey: &ResKey) {
+        println!("  [RECV] RESOURCE ({:?}) ({:?})", rid, reskey);
+    }
+    async fn forget_resource(&self, rid: &ZInt) {
+        println!("  [RECV] FORGET RESOURCE ({:?})", rid);
+    }
+    
+    async fn publisher(&self, reskey: &ResKey) {
+        println!("  [RECV] PUBLISHER ({:?})", reskey);
+    }
+    async fn forget_publisher(&self, reskey: &ResKey) {
+        println!("  [RECV] FORGET PUBLISHER ({:?})", reskey);
+    }
+    
+    async fn subscriber(&self, reskey: &ResKey, mode: &SubMode) {
+        println!("  [RECV] SUBSCRIBER ({:?}) ({:?})", reskey, mode);
+    }
+    async fn forget_subscriber(&self, reskey: &ResKey) {
+        println!("  [RECV] FORGET SUBSCRIBER ({:?})", reskey);
+    }
+    
+    async fn storage(&self, reskey: &ResKey) {
+        println!("  [RECV] STORAGE ({:?})", reskey);
+    }
+    async fn forget_storage(&self, reskey: &ResKey) {
+        println!("  [RECV] FORGET STORAGE ({:?})", reskey);
+    }
+    
+    async fn eval(&self, reskey: &ResKey) {
+        println!("  [RECV] EVAL ({:?})", reskey);
+    }
+    async fn forget_eval(&self, reskey: &ResKey) {
+        println!("  [RECV] FORGET EVAL ({:?})", reskey);
+    }
+
+    async fn data(&self, reskey: &ResKey, _info: &Option<ArcSlice>, _payload: &ArcSlice) {
+        println!("  [RECV] DATA ({:?})", reskey);
+    }
+    async fn query(&self, reskey: &ResKey, predicate: &String, qid: &ZInt, target: &Option<QueryTarget>, consolidation: &QueryConsolidation) {
+        println!("  [RECV] QUERY ({:?}) ({:?}) ({:?}) ({:?}) ({:?})", reskey, predicate, qid, target, consolidation);
+    }
+    async fn reply(&self, qid: &ZInt, source: &ReplySource, replierid: &Option<PeerId>, reskey: &ResKey, _info: &Option<ArcSlice>, _payload: &ArcSlice) {
+        println!("  [RECV] REPLY ({:?}) ({:?}) ({:?}) ({:?})", qid, source, replierid, reskey);
+    }
+    async fn pull(&self, is_final: bool, reskey: &ResKey, pull_id: &ZInt, max_samples: &Option<ZInt>) {
+        println!("  [RECV] PULL ({:?}) ({:?}) ({:?}) ({:?})", is_final, reskey, pull_id, max_samples);
+    }
+
+    async fn close(&self) {
+        println!("  CLOSE");
+    }
+}
+
+fn main() {
+    task::block_on(async{
+        let mut args = std::env::args();
+        args.next(); // skip exe name
+
+        let my_primitives = Arc::new(PrintPrimitives {});
+    
+        let tables = Arc::new(TablesHdl::new());
+
+        let mut pid = vec![0, 0, 0, 0];
+        rand::thread_rng().fill_bytes(&mut pid);
+    
+        let manager = SessionManager::new(0, WhatAmI::Peer, PeerId{id: pid}, 0, tables.clone());
+        let port = match args.next() { Some(port) => {port} None => {"7447".to_string()}};
+        let locator = ["tcp/127.0.0.1:", &port].concat().parse().unwrap();
+        if let Err(_err) = manager.add_locator(&locator, None).await {
+            println!("Unable to open listening port {}!", port);
+            std::process::exit(-1);
+        }
+
+        while let Some(locator) = args.next() {
+            if let Err(_err) =  manager.open_session(&locator.parse().unwrap()).await {
+                println!("Unable to connect to {}!", locator);
+                std::process::exit(-1);
+            }
+        }
+    
+        let primitives = tables.new_primitives(my_primitives).await;
+
+        primitives.subscriber(&"/demo/peer/**".to_string().into(), &SubMode::Push).await;
+
+        let res: ResKey = ["/demo/peer/", &port].concat().to_string().into();
+        loop {
+            println!("[SEND] DATA ({:?})", &res);
+            primitives.data(&res, &None, &ArcSlice::from(vec![1])).await;
+            std::thread::sleep(std::time::Duration::from_millis(1000));
+        }
+    });
+}
---
+++ zenoh-router/src/bin/thr_sub.rs
@@ -0,0 +1,124 @@
+use async_std::task;
+use async_std::sync::{Arc, Mutex};
+use async_trait::async_trait;
+use rand::RngCore;
+use std::time::{SystemTime, UNIX_EPOCH};
+use zenoh_protocol::core::{PeerId, ResKey, ZInt};
+use zenoh_protocol::io::ArcSlice;
+use zenoh_protocol::proto::WhatAmI;
+use zenoh_protocol::proto::{Primitives, SubMode, QueryConsolidation, QueryTarget, ReplySource};
+use zenoh_protocol::session::SessionManager;
+use zenoh_router::routing::tables::TablesHdl;
+
+const N: usize = 100000;
+
+struct Stats {
+    count: usize,
+    start: SystemTime,
+    stop: SystemTime,
+}
+
+impl Stats {
+
+    pub fn print(&self) {
+        let t0 = self.start.duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs()  as f64 
+            + self.start.duration_since(UNIX_EPOCH).expect("Time went backwards").subsec_nanos() as f64 / 1000000000.0;
+        let t1 = self.stop.duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs()  as f64 
+            + self.stop.duration_since(UNIX_EPOCH).expect("Time went backwards").subsec_nanos() as f64 / 1000000000.0;
+        let thpt = N as f64 / (t1 - t0);
+        println!("{} msgs/sec", thpt);
+    }
+}
+
+pub struct ThrouputPrimitives {
+    stats: Mutex<Stats>,
+}
+
+impl ThrouputPrimitives {
+    pub fn new() -> ThrouputPrimitives {
+        ThrouputPrimitives {
+            stats: Mutex::new(Stats {
+                count: 0,
+                start: UNIX_EPOCH,
+                stop: UNIX_EPOCH,
+            })
+        }
+    }
+}
+
+#[async_trait]
+impl Primitives for ThrouputPrimitives {
+
+    async fn resource(&self, _rid: &ZInt, _reskey: &ResKey) {}
+    async fn forget_resource(&self, _rid: &ZInt) {}
+    
+    async fn publisher(&self, _reskey: &ResKey) {}
+    async fn forget_publisher(&self, _reskey: &ResKey) {}
+    
+    async fn subscriber(&self, _reskey: &ResKey, _mode: &SubMode) {}
+    async fn forget_subscriber(&self, _reskey: &ResKey) {}
+    
+    async fn storage(&self, _reskey: &ResKey) {}
+    async fn forget_storage(&self, _reskey: &ResKey) {}
+    
+    async fn eval(&self, _reskey: &ResKey) {}
+    async fn forget_eval(&self, _reskey: &ResKey) {}
+
+    async fn data(&self, _reskey: &ResKey, _info: &Option<ArcSlice>, _payload: &ArcSlice) {
+        let mut stats = self.stats.lock().await;
+        if stats.count == 0 {
+            stats.start = SystemTime::now();
+            stats.count += 1;
+        } else if stats.count < N {
+            stats.count += 1;
+        } else {
+            stats.stop = SystemTime::now();
+            stats.print();
+            stats.count = 0;
+        }  
+    }
+    async fn query(&self, _reskey: &ResKey, _predicate: &String, _qid: &ZInt, _target: &Option<QueryTarget>, _consolidation: &QueryConsolidation) {}
+    async fn reply(&self, _qid: &ZInt, _source: &ReplySource, _replierid: &Option<PeerId>, _reskey: &ResKey, _info: &Option<ArcSlice>, _payload: &ArcSlice) {}
+    async fn pull(&self, _is_final: bool, _reskey: &ResKey, _pull_id: &ZInt, _max_samples: &Option<ZInt>) {}
+
+    async fn close(&self) {}
+}
+
+fn main() {
+    task::block_on(async{
+        let mut args = std::env::args();
+        args.next(); // skip exe name
+
+        let my_primitives = Arc::new(ThrouputPrimitives::new());
+    
+        let tables = Arc::new(TablesHdl::new());
+
+        let mut pid = vec![0, 0, 0, 0];
+        rand::thread_rng().fill_bytes(&mut pid);
+    
+        let manager = SessionManager::new(0, WhatAmI::Peer, PeerId{id: pid}, 0, tables.clone());
+        let port = match args.next() { Some(port) => {port} None => {"7447".to_string()}};
+        let locator = ["tcp/127.0.0.1:", &port].concat().parse().unwrap();
+        if let Err(_err) = manager.add_locator(&locator, None).await {
+            println!("Unable to open listening port {}!", port);
+            std::process::exit(-1);
+        }
+
+        while let Some(locator) = args.next() {
+            if let Err(_err) =  manager.open_session(&locator.parse().unwrap()).await {
+                println!("Unable to connect to {}!", locator);
+                std::process::exit(-1);
+            }
+        }
+    
+        let primitives = tables.new_primitives(my_primitives).await;
+
+        primitives.resource(&1, &"/tp".to_string().into()).await;
+        let rid = ResKey::ResId{id:1};
+        primitives.subscriber(&rid, &SubMode::Push).await;
+
+        loop {
+            std::thread::sleep(std::time::Duration::from_millis(10000));
+        }
+    });
+}
