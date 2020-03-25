bebc85d2ed0dfa2706d980bbe0d9bc3b6ffcc8c0
kydos <angelo@icorsaro.net>
2020-03-25 17:27:34 +0100

    editorial fixes

--- zenoh/examples/zenoh-net/zn_pub_res.rs
+++ zenoh/examples/zenoh-net/zn_pub_res.rs
@@ -1,7 +1,6 @@
 use std::env;
 use async_std::task;
 use zenoh::net::*;
-// use zenoh::net::ResKey::*;
 
 fn main() {
     task::block_on( async {
@@ -14,18 +13,11 @@ fn main() {
 
         println!("Openning session...");
         let session = open(&locator, None).await.unwrap();
-
-        // Split the uri on the last '/'. 
-        // The first part will be declared as resource,
-        // and the second as suffix for writing using the resource.
-        // let mut resource_name = uri.to_string();
-        // let suffix = resource_name.split_off(resource_name.rfind('/').unwrap());
-
+        
         println!("Declaring Resource {} ", uri);
-
         let rid = session.declare_resource(&uri.into()).await.unwrap();
+        
         println!("Declaring Publisher on {}", rid);
-
         let res_key = rid.into();
         let publ = session.declare_publisher(&res_key).await.unwrap();
         
