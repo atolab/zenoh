438031f1be769db3cb3134a4f1cb59f6100508e9
kydos <angelo@icorsaro.net>
2020-03-25 10:38:41 +0100

    added example on circular queue that uses multiple producers and consumer

--- zenoh-util/examples/async_queue.rs
+++ zenoh-util/examples/async_queue.rs
@@ -5,26 +5,40 @@ use std::sync::Arc;
 use std::time::Instant;
 
 fn main() {    
-  task::block_on(async {        
-      let acb = Arc::new(CircularQueue::<u64>::new(256, 16));
-      let p = acb.clone();
-      let c = acb.clone();
-      let now = Instant::now();
-      let f1 = task::spawn(async move  {
-          for i in 0..10_000_000u32 {                
-              p.push(i as u64).await;                
-          }
-      });
+    task::block_on(async {        
+        let acb = Arc::new(CircularQueue::<u64>::new(256, 16));
+        let cq1 = acb.clone();
+        let cq2 = acb.clone();
+        let cq3 = acb.clone();
+        let cq4 = acb.clone();
+        let now = Instant::now();
+        let n = 10_000_000u32;
 
-      let f2 = task::spawn(async move {
-          for  _ in 0..10_000_000u32 {
-              let _ = c.pull().await; 
-           
-          }
+        let p1 = task::spawn(async move  {
+            for i in 0..n {                
+                cq1.push(i as u64).await;                
+            }         
+        });
+
+        let p2 = task::spawn(async move  {
+            for i in 0..n {                
+                cq2.push(i as u64).await;                
+            }
+        });        
         
-      });
+        let c1 = task::spawn(async move {
+            for  _ in 0..n {
+                let _ = cq3.pull().await;            
+            }        
+        });
 
-      join!(f2, f1);
+        let c2 = task::spawn(async move {
+            for  _ in 0..n {
+                let _ = cq4.pull().await;            
+            }        
+        });
+
+      join!(p1, p2, c1, c2);
       println!("Test run in: {}", now.elapsed().as_millis());
   });
 }
