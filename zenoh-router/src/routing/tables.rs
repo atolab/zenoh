7d1b3a26e3aa43199d6421bbbf6312717c374072
Olivier Hécart <olivier.hecart@adlinktech.com>
2020-05-13 10:32:29 +0200

    Routing code reorg

--- zenoh-router/src/routing/tables.rs
+++ zenoh-router/src/routing/tables.rs
@@ -2,7 +2,6 @@ use async_trait::async_trait;
 use async_std::sync::RwLock;
 use std::sync::{Arc, Weak};
 use std::collections::{HashMap};
-use zenoh_protocol::core::rname::intersect;
 use zenoh_protocol::core::{ResKey, ZInt};
 use zenoh_protocol::io::RBuf;
 use zenoh_protocol::proto::{Primitives, SubInfo, SubMode, Reliability, Mux, DeMux, WhatAmI, QueryTarget, QueryConsolidation, Reply};
@@ -216,7 +215,7 @@ impl Tables {
         for match_ in &res.matches {
             for (sid, context) in &match_.upgrade().unwrap().contexts {
                 if context.subs.is_some() {
-                    let (rid, suffix) = Tables::get_best_key(res, "", *sid);
+                    let (rid, suffix) = Resource::get_best_key(res, "", *sid);
                     dests.insert(*sid, (context.face.clone(), rid, suffix));
                 }
             }
@@ -235,29 +234,6 @@ impl Tables {
         }
     }
 
-    unsafe fn make_and_match_resource(from: &Arc<Resource>, prefix: &mut Arc<Resource>, suffix: &str) -> Arc<Resource> {
-        let mut res = Resource::make_resource(prefix, suffix);
-        let mut matches = Tables::get_matches_from(&res.name(), from);
-
-        fn matches_contain(matches: &[Weak<Resource>], res: &Arc<Resource>) -> bool {
-            for match_ in matches {
-                if Arc::ptr_eq(&match_.upgrade().unwrap(), res) {
-                    return true
-                }
-            }
-            false
-        }
-        
-        for match_ in &mut matches {
-            let mut match_ = match_.upgrade().unwrap();
-            if ! matches_contain(&match_.matches, &res) {
-                Arc::get_mut_unchecked(&mut match_).matches.push(Arc::downgrade(&res));
-            }
-        }
-        Arc::get_mut_unchecked(&mut res).matches = matches;
-        res
-    }
-
     pub async fn declare_resource(tables: &Arc<RwLock<Tables>>, sex: &Weak<Face>, rid: u64, prefixid: u64, suffix: &str) {
         let t = tables.write().await;
         match sex.upgrade() {
@@ -271,7 +247,8 @@ impl Tables {
                     None => {
                         match t.get_mapping(&sex, &prefixid).cloned() {
                             Some(mut prefix) => unsafe {
-                                let mut res = Tables::make_and_match_resource(&t.root_res, &mut prefix, suffix);
+                                let mut res = Resource::make_resource(&mut prefix, suffix);
+                                Resource::match_resource(&t.root_res, &mut res);
                                 let mut ctx = Arc::get_mut_unchecked(&mut res).contexts.entry(sex.id).or_insert_with( ||
                                     Arc::new(Context {
                                         face: sex.clone(),
@@ -322,7 +299,8 @@ impl Tables {
             Some(mut sex) => {
                 match t.get_mapping(&sex, &prefixid).cloned() {
                     Some(mut prefix) => unsafe {
-                        let mut res = Tables::make_and_match_resource(&t.root_res, &mut prefix, suffix);
+                        let mut res = Resource::make_resource(&mut prefix, suffix);
+                        Resource::match_resource(&t.root_res, &mut res);
                         {
                             let res = Arc::get_mut_unchecked(&mut res);
                             match res.contexts.get_mut(&sex.id) {
@@ -429,7 +407,8 @@ impl Tables {
                 };
                 match prefix {
                     Some(mut prefix) => unsafe {
-                        let mut res = Tables::make_and_match_resource(&t.root_res, &mut prefix, suffix);
+                        let mut res = Resource::make_resource(&mut prefix, suffix);
+                        Resource::match_resource(&t.root_res, &mut res);
                         {
                             match Arc::get_mut_unchecked(&mut res).contexts.get_mut(&sex.id) {
                                 Some(mut ctx) => {
@@ -523,81 +502,9 @@ impl Tables {
         }
     }
 
-    fn fst_chunk(rname: &str) -> (&str, &str) {
-        if rname.starts_with('/') {
-            match rname[1..].find('/') {
-                Some(idx) => {(&rname[0..(idx+1)], &rname[(idx+1)..])}
-                None => (rname, "")
-            }
-        } else {
-            match rname.find('/') {
-                Some(idx) => {(&rname[0..(idx)], &rname[(idx)..])}
-                None => (rname, "")
-            }
-        }
-    }
-
-    fn get_matches_from(rname: &str, from: &Arc<Resource>) -> Vec<Weak<Resource>> {
-        let mut matches = Vec::new();
-        if from.parent.is_none() {
-            for child in from.childs.values() {
-                matches.append(&mut Tables::get_matches_from(rname, child));
-            }
-            return matches
-        }
-        if rname.is_empty() {
-            if from.suffix == "/**" || from.suffix == "/" {
-                matches.push(Arc::downgrade(from));
-                for child in from.childs.values() {
-                    matches.append(&mut Tables::get_matches_from(rname, child));
-                }
-            }
-            return matches
-        }
-        let (chunk, rest) = Tables::fst_chunk(rname);
-        if intersect(chunk, &from.suffix) {
-            if rest.is_empty() || rest == "/" || rest == "/**" {
-                matches.push(Arc::downgrade(from));
-            } else if chunk == "/**" || from.suffix == "/**" {
-                matches.append(&mut Tables::get_matches_from(rest, from));
-            }
-            for child in from.childs.values() {
-                matches.append(&mut Tables::get_matches_from(rest, child));
-                if chunk == "/**" || from.suffix == "/**" {
-                    matches.append(&mut Tables::get_matches_from(rname, child));
-                }
-            }
-        }
-        matches
-    }
-
     pub async fn get_matches(tables: &Arc<RwLock<Tables>>, rname: &str) -> Vec<Weak<Resource>> {
         let t = tables.read().await;
-        Tables::get_matches_from(rname, &t.root_res)
-    }
-
-    #[inline]
-    fn get_best_key(prefix: &Arc<Resource>, suffix: &str, sid: usize) -> (u64, String) {
-        fn get_best_key_(prefix: &Arc<Resource>, suffix: &str, sid: usize, checkchilds: bool) -> (u64, String) {
-            if checkchilds && ! suffix.is_empty() {
-                let (chunk, rest) = Tables::fst_chunk(suffix);
-                if let Some(child) = prefix.childs.get(chunk) {
-                    return get_best_key_(child, rest, sid, true)
-                }
-            }
-            if let Some(ctx) = prefix.contexts.get(&sid) {
-                if let Some(rid) = ctx.local_rid {
-                    return (rid, suffix.to_string())
-                } else if let Some(rid) = ctx.remote_rid {
-                    return (rid, suffix.to_string())
-                }
-            }
-            match &prefix.parent {
-                Some(parent) => {get_best_key_(&parent, &[&prefix.suffix, suffix].concat(), sid, false)}
-                None => {(0, suffix.to_string())}
-            }
-        }
-        get_best_key_(prefix, suffix, sid, true)
+        Resource::get_matches_from(rname, &t.root_res)
     }
 
     pub async fn route_data_to_map(tables: &Arc<RwLock<Tables>>, sex: &Weak<Face>, rid: u64, suffix: &str) -> Option<DataRoute> {
@@ -611,12 +518,12 @@ impl Tables {
                             Some(res) => {Some(res.route.clone())}
                             None => {
                                 let mut sexs = HashMap::new();
-                                for res in Tables::get_matches_from(&[&prefix.name(), suffix].concat(), &t.root_res) {
+                                for res in Resource::get_matches_from(&[&prefix.name(), suffix].concat(), &t.root_res) {
                                     let res = res.upgrade().unwrap();
                                     for (sid, context) in &res.contexts {
                                         if context.subs.is_some() {
                                             sexs.entry(*sid).or_insert_with( || {
-                                                let (rid, suffix) = Tables::get_best_key(prefix, suffix, *sid);
+                                                let (rid, suffix) = Resource::get_best_key(prefix, suffix, *sid);
                                                 (context.face.clone(), rid, suffix)
                                             });
                                         }
@@ -670,14 +577,14 @@ impl Tables {
                     Some(prefix) => {
                         let query = Arc::new(Query {src_face: sex.clone(), src_qid: qid});
                         let mut sexs = HashMap::new();
-                        for res in Tables::get_matches_from(&[&prefix.name(), suffix].concat(), &t.root_res) {
+                        for res in Resource::get_matches_from(&[&prefix.name(), suffix].concat(), &t.root_res) {
                             unsafe {
                                 let mut res = res.upgrade().unwrap();
                                 for (sid, context) in &mut Arc::get_mut_unchecked(&mut res).contexts {
                                     if context.qabl && ! Arc::ptr_eq(&sex, &context.face)
                                     {
                                         sexs.entry(*sid).or_insert_with( || {
-                                            let (rid, suffix) = Tables::get_best_key(prefix, suffix, *sid);
+                                            let (rid, suffix) = Resource::get_best_key(prefix, suffix, *sid);
                                             let face = Arc::get_mut_unchecked(&mut Arc::get_mut_unchecked(context).face);
                                             face.next_qid += 1;
                                             let qid = face.next_qid;
